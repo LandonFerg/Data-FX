070c068ddf55c641e72872435a53ddbf1e79d2f7
LandonFerg <landonljf@gmail.com>
2019-05-12 22:22:58 -0400

    File selection

--- csv_op.py
+++ csv_op.py
@@ -1,4 +1,6 @@
 import bpy
+import csv
+import os
 
 class OT_Load_CSV(bpy.types.Operator):
     bl_idname = "view3d.do_stuff"
@@ -11,14 +13,22 @@ class OT_Load_CSV(bpy.types.Operator):
     
 
     def execute(self, context):
-        if(self.filepath.endswith('.csv')):
-            print(self.filepath)
-            # Do something with file
+        if(self.filepath.endswith('.csv')): # File is CSV
+            csvFile = self.filepath
+
+            with open (csvFile, 'rt') as f: # Iterate through CSV
+                reader = csv.reader(f)
+                next(reader) # Skip headers
+                for row in reader:
+                    print(row[0])
+
         else:
             print("Error: File not a CSV type")
+            self.report({'ERROR_INVALID_INPUT'}, "File not of type CSV")
+            return{'CANCELLED'}
 
         return {'FINISHED'}
 
-    def invoke(self, context, event):
+    def invoke(self, context, event):   # File select
         context.window_manager.fileselect_add(self)
         return {'RUNNING_MODAL'}
